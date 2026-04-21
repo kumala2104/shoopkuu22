4bab4e9dd11d1ede90d221b46917e85a3fb87927
Leo Kuma <emailkamu@gmail.com>
2026-04-21 13:39:53 +0700

    add migration files for roles, categories, items, orders, and order_items tables

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -13,12 +14,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name')->nullable()->fillable();
+            $table->string('password')->nullable();
+            $table->string('fullname');
+            $table->string('email')->unique()->fillable();
+            $table->string('phone');
+            $table->unsignedBigInteger('role_id');
+            $table->softDeletes();
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
